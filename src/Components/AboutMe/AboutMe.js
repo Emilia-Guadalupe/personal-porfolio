04c2579599179fd8adcf6219059627f016287e07
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-16 19:12:43 -0300

    Editing...

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -8,8 +8,8 @@ function AboutMe() {
     return(
         <div className="about-me-container">
             <div className="about-me-part">
-                <h2>Sobre Mi</h2>
-                <p>Lorem ipsum dolor sit amet. Loren ipsum loren loren impsum ipsum</p>
+                <h2 className="second-title">Sobre mi</h2>
+                <p className="about-me-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             </div>
             <div className="about-me-part">
                 <img className="image_drawing" src={ProfileDrawing} alt="profile drawing"/>
