3a7e0f12941e89c6670d88cab07f860f043d3e5f
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-16 13:14:28 -0300

    Added a new component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 //Components
 import Header from './Components/Header/Header';
 import Name from './Components/Name/Name';
+import AboutMe from './Components/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Name />
+      <AboutMe />
     </div>
   );
 }
