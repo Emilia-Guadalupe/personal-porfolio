ce0e8d31147e9b8467d15f5736f357645beff4b9
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-19 19:56:11 -0300

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 //Components
 import Header from './Components/Header/Header';
 import Name from './Components/Name/Name';
 import AboutMe from './Components/AboutMe/AboutMe';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
       <Header />
-      <Name />
-      <AboutMe />
+        <Switch>
+        <Route exact path="/">
+            <Name />
+            <AboutMe />
+        </Route>
+        </Switch>
+      <Footer />
     </div>
+  </BrowserRouter>
   );
 }
 
