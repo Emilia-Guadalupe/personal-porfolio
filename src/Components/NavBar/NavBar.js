2ec6073022cbc4796827f30cb15a3654f1fa99cd
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-21 19:28:59 -0300

    Added links

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavBarWrapper } from './NavBarStyles';
+import { Link } from 'react-router-dom';
 
 function NavBar({open}){
     return(
         <NavBarWrapper open={open}>
-            <p className="link_styles">Link</p>
+            <Link className="link_styles" to="/my-stack">Mi Stack</Link>
             <p className="link_styles">Link</p>
             <p className="link_styles">Link</p>
         </NavBarWrapper>
