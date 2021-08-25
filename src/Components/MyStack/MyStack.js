2137ea4dba17daa09b35040d78a7d5320e7d6acd
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-25 08:54:28 -0300

    Making some changes

--- src/Components/MyStack/MyStack.js
+++ src/Components/MyStack/MyStack.js
@@ -3,10 +3,15 @@ import './myStackStyles.css';
 
 function MyStack() {
     return(
+        <>
         <div className="my-stack-container">
             <h3>Mi Stack y tecnologías</h3>
-            <p className="about-my-stack"></p>
+            <p className="about-my-stack">Empecé mi formación como Desarrolladora especializándome en Front End y actualmente me encuentro estudiando Node.js y Express, para poder contar con conocimientos de Back End</p>
         </div>
+        <div className="stack-list-container">
+            
+        </div>
+        </>
     )
 }
 
