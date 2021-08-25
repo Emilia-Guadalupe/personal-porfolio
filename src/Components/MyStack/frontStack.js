616bcdca6cf0f3c7d1c76c348931545a8f80dc9a
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-25 08:50:21 -0300

    Front End Stack :)

---
+++ src/Components/MyStack/frontStack.js
@@ -0,0 +1,33 @@
+export const frontStack = [
+    {
+        "id": 0,
+        "name": "HTML",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/230px-HTML5_logo_and_wordmark.svg.png'
+    },
+    {
+        "id": 1,
+        "name": "CSS",
+        "img": 'https://desarrolloweb.com/storage/tag_images/actual/sT1RLpDHzInATuKnDUkwXhKoaIOrtS97gBtgiQ6M.png'
+    },
+    {
+        "id": 2,
+        "name": "SASS",
+        "img": 'https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg'
+    },
+    {
+        "id": 3,
+        "name": "Javascript",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/245px-Unofficial_JavaScript_logo_2.svg.png'
+    },
+    {
+        "id": 4,
+        "name": "React.js",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/250px-React.svg.png'
+    },
+    {
+        "id": 5,
+        "name": "Vue.js",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/250px-Vue.js_Logo_2.svg.png'
+    }
+]
+
