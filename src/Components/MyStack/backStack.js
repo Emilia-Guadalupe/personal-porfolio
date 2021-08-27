ea859f19f9b2a0ed88d282d77dabb82ba7dd9afd
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-27 08:52:29 -0300

    Edited the files

--- src/Components/MyStack/backStack.js
+++ src/Components/MyStack/backStack.js
@@ -1,10 +1,12 @@
 export const backStack = [
     {
-        "id": 1,
-        "name": "Node.js"
+        "id": 7,
+        "name": "Node.js",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/200px-Node.js_logo.svg.png'
     },
     {
-        "id": 2,
-        "name": "Express"
+        "id": 8,
+        "name": "Express",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/220px-Expressjs.png'
     }
 ]
--- src/Components/MyStack/frontStack.js
+++ src/Components/MyStack/frontStack.js
@@ -1,31 +1,31 @@
 export const frontStack = [
     {
-        "id": 0,
+        "id": 1,
         "name": "HTML",
         "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/230px-HTML5_logo_and_wordmark.svg.png'
     },
     {
-        "id": 1,
+        "id": 2,
         "name": "CSS",
         "img": 'https://desarrolloweb.com/storage/tag_images/actual/sT1RLpDHzInATuKnDUkwXhKoaIOrtS97gBtgiQ6M.png'
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "SASS",
         "img": 'https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg'
     },
     {
-        "id": 3,
+        "id": 4,
         "name": "Javascript",
         "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/245px-Unofficial_JavaScript_logo_2.svg.png'
     },
     {
-        "id": 4,
+        "id": 5,
         "name": "React.js",
         "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/250px-React.svg.png'
     },
     {
-        "id": 5,
+        "id": 6,
         "name": "Vue.js",
         "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/250px-Vue.js_Logo_2.svg.png'
     }
