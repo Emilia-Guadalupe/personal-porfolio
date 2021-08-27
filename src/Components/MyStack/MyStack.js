c696043d503c69d96f636f41d57f587d5d90c184
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-27 08:52:45 -0300

    Changed the Component

--- src/Components/MyStack/MyStack.js
+++ src/Components/MyStack/MyStack.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './myStackStyles.css';
 
+//Components
+import StackList from './StackList';
+
 //Helpers
 import { frontStack } from './frontStack';
 import { backStack } from './backStack';
@@ -14,7 +17,29 @@ function MyStack() {
                 <p className="about-my-stack">Empecé mi formación como Desarrolladora especializándome en Front End y actualmente me encuentro estudiando Node.js y Express, para poder contar con conocimientos de Back End</p>
             </div>
             <div className="stack-list-container">
-            <>
+                <div className="front-stack-container">
+                    {frontStack.map((e) => {
+                        return(
+                            <StackList id={e.id} name={e.name} img={e.img} />
+                        )
+                    })}
+                </div>
+                <div className="back-stack-container">
+                    {backStack.map((e) => {
+                        return(
+                            <StackList id={e.id} name={e.name} img={e.img} />
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyStack;
+
+
+/*<>
                 {frontStack.map((e) =>  {
                     return(
                         <div key={e.id} className="stack-container">
@@ -32,10 +57,4 @@ function MyStack() {
                         </div>
                     )
                 })}
-            </>
-            </div>
-        </div>
-    )
-}
-
-export default MyStack;
+            </>*/ 
