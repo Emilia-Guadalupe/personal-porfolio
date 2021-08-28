fcd65b45108d6b91486653ada971bee09eb04efb
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-28 19:22:09 -0300

    Adding Tech List

---
+++ src/Components/MyStack/techStack.js
@@ -0,0 +1,17 @@
+export const techStack = [
+    {
+        "id": 9,
+        "name": "Git",
+        "img": 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/200px-Git-logo.svg.png'
+    },
+    {
+        "id": 10,
+        "name": "Github",
+        "img": 'https://midu.dev/images/tags/github.png'
+    },
+    {
+        "id": 11,
+        "name": "Firebase",
+        "img": 'https://firebase.google.com/images/homepage/cloud-plus-firebase_1x.png?hl=es-419'
+    }
+]
