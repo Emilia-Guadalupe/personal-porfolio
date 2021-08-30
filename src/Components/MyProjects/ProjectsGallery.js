3c8734fffc1c0c3c2246d58e189578802e26ff5d
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-29 21:53:39 -0300

    New Gallery!

--- src/Components/MyProjects/ProjectsGallery.js
+++ src/Components/MyProjects/ProjectsGallery.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+//Components
+import Project from './Project';
+
+//Icons
+import Github from '../../assets/GitHub-Mark-32px.png';
+import Link from '../../assets/icons8-link-30.png';
+
+//Images
+import ProjectOne from '../../assets/Quorum_Project.jpg';
+
 function ProjectsGallery() {
     return(
-        <div>
-            
+        <div className="projects-gallery">
+            <Project project={ProjectOne} />
         </div>
     )
 }
