66f0f1b23962e339901caf50c5761f801c6dced9
Emilia <m.emilia.guadalupe@gmail.com>
2021-08-29 21:53:22 -0300

    Trying something...

---
+++ src/Components/MyProjects/projectsArray.js
@@ -0,0 +1,10 @@
+export const projectsArray = [
+    {
+        "id": "AA",
+        "img": './assets/Quorum_Project.jpg',
+        "githubIcon": './assets/GitHub-Mark-32px.png',
+        "githubLink": 'https://github.com/Emilia-Guadalupe/Quorum-iTeam-proyect',
+        "linkIcon": './assets/icons8-link-30.png',
+        "link": 'https://quorum-i-team-proyect.vercel.app/'
+    }
+] 
