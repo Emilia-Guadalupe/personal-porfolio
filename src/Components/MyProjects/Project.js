81c19432061d28024841debf6e358cc22bfb1f8a
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-01 09:51:39 -0300

    Adding projects

--- src/Components/MyProjects/Project.js
+++ src/Components/MyProjects/Project.js
@@ -3,21 +3,21 @@ import './projectStyles.css';
 
 function Project({photo, github, githubLink, linkIcon, pageLink}) {
     return(
-        <>
-            <div className="projects-container">
-                <img className="project" src={photo} alt="Proyecto" />
-            </div>
+        <div className="total-project-container">
             <div className="hover-container">
                 <div className="github-container">
-                    <img src={github} alt="github icon" />
+                    <img src={github} alt="github icon" className="github-icon" />
                     <a href={githubLink} target="_blank" rel="noreferrer">Ver en Github</a>
                 </div>
                 <div className="link-container">
-                    <img src={linkIcon} alt="link Icon" />
+                    <img src={linkIcon} alt="link Icon" className="link-icon" />
                     <a href={pageLink} target="_blank" rel="noreferrer">Ver el proyecto</a>
                 </div>
             </div>
-        </>
+            <div className="projects-container">
+                <img className="project" src={photo} alt="Proyecto" />
+            </div>
+        </div>
     )
 }
 
