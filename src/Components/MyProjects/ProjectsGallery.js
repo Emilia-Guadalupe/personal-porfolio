8a36fa96d8721d45a88259191a8e8882144ac69d
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-01 09:51:56 -0300

    Adding projects

--- src/Components/MyProjects/ProjectsGallery.js
+++ src/Components/MyProjects/ProjectsGallery.js
@@ -10,12 +10,22 @@ import LinkIcon from '../../Images/linkIcon.png';
 
 //Images
 import QuorumProject from '../../Images/quorumProject.jpg';
+import EnLaNubeProject from '../../Images/EnLaNubeProject.jpg';
+import ToDoListProject from '../../Images/ToDoListProject.jpg';
+import PokeProject from '../../Images/PokeProject.jpg';
 
 function ProjectsGallery() {
 
     return(
         <div className="projects-gallery">
-            <Project photo={QuorumProject} github={GithubIcon} linkIcon={LinkIcon} githubLink="https://github.com/Emilia-Guadalupe/Quorum-iTeam-proyect" pageLink="https://quorum-i-team-proyect.vercel.app/" />
+            <div>
+                <Project photo={QuorumProject} github={GithubIcon} linkIcon={LinkIcon} githubLink="https://github.com/Emilia-Guadalupe/Quorum-iTeam-proyect" pageLink="https://quorum-i-team-proyect.vercel.app/" />
+                <Project photo={EnLaNubeProject} github={GithubIcon} linkIcon={LinkIcon} githubLink="https://github.com/Emilia-Guadalupe/React-Proyect" pageLink="https://wonderful-snyder-c0c791.netlify.app/" />
+            </div>
+            <div>
+                <Project photo={ToDoListProject} github={GithubIcon} linkIcon={LinkIcon} githubLink="https://github.com/Emilia-Guadalupe/To-Do-List-App" pageLink="https://inspiring-wright-0c51b2.netlify.app" />
+                <Project photo={PokeProject} github={GithubIcon} linkIcon={LinkIcon} githubLink="https://github.com/Emilia-Guadalupe/PokeReact" pageLink="https://stupefied-swirles-51325b.netlify.app/" />
+            </div>
         </div>
     )
 }
