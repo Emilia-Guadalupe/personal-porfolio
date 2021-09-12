d0969cf81da1e213c638368bbfc2596b8432ab2d
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-12 18:26:49 -0300

    Deleting Components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import './App.css';
 
 //Components
 import Header from './Components/Header/Header';
-import Name from './Components/Name/Name';
-import AboutMe from './Components/AboutMe/AboutMe';
 import Footer from './Components/Footer/Footer';
-import MyStack from './Components/MyStack/MyStack';
-import MyProjects from './Components/MyProjects/MyProjects';
-import ProjectsGallery from './Components/MyProjects/ProjectsGallery';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Name />
-      <AboutMe />
       <Footer />
     </div>
   );
