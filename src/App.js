fe27e0903e35720bd308408c3e554e54c2049560
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-14 09:04:32 -0300

    New Components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //Components
 import Header from './Components/Header/Header';
 import Name from './Components/Name/Name';
+import AboutMe from './Components/AboutMe/AboutMe';
 import Footer from './Components/Footer/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Name />
+      <AboutMe />
       <Footer />
     </div>
   );
