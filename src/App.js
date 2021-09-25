975dd577beb213bce7694736cbb7781dc055dcad
Emilia <m.emilia.guadalupe@gmail.com>
2021-09-25 19:49:01 -0300

    Adding components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Components/Header/Header';
 import Name from './Components/Name/Name';
 import AboutMe from './Components/AboutMe/AboutMe';
 import MyStack from './Components/MyStack/MyStack';
+import MyProjects from './Components/MyProjects/MyProjects';
 import Footer from './Components/Footer/Footer';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Name />
       <AboutMe />
       <MyStack />
+      <MyProjects />
       <Footer />
     </div>
   );
